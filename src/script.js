1bd63890687846abc9738a30a325af3d80b3b555
IsauraCardenas <isauracardenas9@gmail.com>
2024-05-23 11:14:25 -0400

    Added axios and AI API

--- src/script.js
+++ src/script.js
@@ -1,12 +1,27 @@
-function generatorPoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "Podrá nublarse el sol eternamente;",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatorPoem(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#users-instruction");
+  let apiKey = "8df22e207cb3offb32384a26df4ddft4";
+  let prompt = `Generate a Spanish poem about ${instructionInput.value}`;
+  let context =
+    "You are a romantic poem expert that love to write short poems. Please generate a 4 line poem following users instructions. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(`Prompt is ${prompt}`);
+  console.log(`Context is ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
