bd19c2bb21f778fe51acad63d6a4cc8121d97134
IsauraCardenas <isauracardenas9@gmail.com>
2024-05-23 20:33:23 -0400

    Changed the content of the website

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-function displayPoem(response) {
-  console.log("poem generated");
-  new Typewriter("#poem", {
+function displayDesserts(response) {
+  console.log("desserts generated");
+  new Typewriter("#desserts", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -8,25 +8,25 @@ function displayPoem(response) {
   });
 }
 
-function generatorPoem(event) {
+function generatorDesserts(event) {
   event.preventDefault();
 
   let instructionInput = document.querySelector("#users-instruction");
   let apiKey = "8df22e207cb3offb32384a26df4ddft4";
-  let prompt = `Generate a Spanish poem about ${instructionInput.value}`;
+  let prompt = `Generate one dessert idea using ${instructionInput.value} `;
   let context =
-    "You are a romantic poem expert that love to write short poems. Please generate a 4 line poem following users instructions. ";
+    "You are a chef, creating delicious easy to-make desserts. Please include a LIST of ingredients and instructions on how to make. Please, make it short.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">Generating a Spanish poem about ${instructionInput.value}</div>`;
+  let dessertsElement = document.querySelector("#desserts");
+  dessertsElement.classList.remove("hidden");
+  dessertsElement.innerHTML = `<div class="generating">Generating ${instructionInput.value}</div>`;
 
   console.log(`Prompt is ${prompt}`);
   console.log(`Context is ${context}`);
 
-  axios.get(apiUrl).then(displayPoem);
+  axios.get(apiUrl).then(displayDesserts);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatorPoem);
+let dessertsFormElement = document.querySelector("#desserts-generator-form");
+dessertsFormElement.addEventListener("submit", generatorDesserts);
